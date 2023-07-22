f0bece4f908325478ad323fef08d3306a894104c
KevinHuanggg <qw820301@gmail.com>
2023-07-23 01:54:28 +0800

    完成ProgressControl.jsx的HTML架構。

--- src/components/Main/ProgressControl/ProgressControl.jsx
+++ src/components/Main/ProgressControl/ProgressControl.jsx
@@ -1,5 +1,42 @@
+import { ReactComponent as LeftArrow } from "../../../icons/arrow-left.svg";
+import { ReactComponent as RightArrow } from "../../../icons/arrow-right.svg";
 import styles from "./ProgressControl.module.css";
 
 export default function ProgressControl() {
-  return <div className={styles.progress}>ProgressControl</div>;
+  return (
+    <section class={styles.progressControlContainer}>
+      <section
+        class={`${styles.buttonGroup} ${styles.buttonGroup1}`}
+        data-phase="address"
+      >
+        <button class={styles.next}>
+          下一步
+          <RightArrow className={styles.arrowRight} />
+        </button>
+      </section>
+      <section
+        class={`${styles.buttonGroup} ${styles.buttonGroup2}`}
+        data-phase="shipping"
+      >
+        <button class={styles.prev}>
+          <LeftArrow className={styles.arrowLeft} />
+          上一步
+        </button>
+        <button class={styles.next}>
+          下一步
+          <RightArrow className={styles.arrowRight} />
+        </button>
+      </section>
+      <section
+        class={`${styles.buttonGroup} ${styles.buttonGroup3}`}
+        data-phase="credit-card"
+      >
+        <button class={styles.prev}>
+          <LeftArrow className={styles.arrowLeft} />
+          上一步
+        </button>
+        <button class={styles.next}>確認下單</button>
+      </section>
+    </section>
+  );
 }
